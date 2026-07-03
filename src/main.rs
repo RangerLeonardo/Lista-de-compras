1ab1321174cb53dc2bdc39110f1b0022f5108bb4
RangerLeonardo <brandonleonardobarrera@gmail.com>
2026-07-03 15:06:46 -0600

    First hola mundo en Rust usando Axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+//Importamos las librerias a usar
+use askama::Template;
+use askama_web::WebTemplate; 
+use axum::{response::IntoResponse, routing::get, Router};
+
+// Al agregar WebTemplate al derive, se implementa automáticamente IntoResponse
+#[derive(Template, WebTemplate)]
+#[template(path = "inicio.html")]
+struct InicioTemplate {
+    establecimiento: String,
+}
+
+async fn handler_inicio() -> impl IntoResponse {
+    InicioTemplate {
+        establecimiento: "Amazon, Soriana y el Mercado".to_string(),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler_inicio));
+
+    let direccion = "127.0.0.1:3000";
+    let listener = tokio::net::TcpListener::bind(direccion).await.unwrap();
+    println!("🚀 Servidor web listo en http://{}", direccion);
+
+    axum::serve(listener, app).await.unwrap();
 }
